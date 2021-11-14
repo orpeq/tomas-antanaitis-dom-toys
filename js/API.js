daa6e069f0eafac6b3e036e08f067ec77cf1d209
Tomas Antanaitis <to.antanaitis@gmail.com>
2021-11-14 16:31:41 +0200

    5: API  servisas

---
+++ js/API.js
@@ -0,0 +1,27 @@
+const baseURL = 'http://localhost:3000';
+
+class API {
+    static fetchToys = (success, failure) => {
+        fetch(`${baseURL}/toys`)
+        .then(res => res.json())
+        .then(success)
+        .catch(failure)
+    }
+
+    static deleteToy = (id, success, failure) => {
+        fetch(`${baseURL}/toys/${id}`, { method: 'DELETE' })
+        .then(res => res.ok ? success() : failure(res.statusText))
+        .catch(failure)
+    }
+}
+
+// API.fetchToys(
+//     console.log,
+//     console.err
+// )
+
+// API.deleteToy(
+//     "2",
+//     () => console.log('Ištrinta sėkmingai'),
+//     console.err
+// )
